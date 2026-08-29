7203351454b9c08bfc4b101875c981067bcba8d2
kaungmyatjapan1999-boop <kaungmyatjapan1999@gmail.com>
2026-08-29 12:40:13 +0700

    Add Kotlin Compose plugin and Google Services plugin

--- V2rayNG/build.gradle.kts
+++ V2rayNG/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false // Firebase Google Services Plugin (Project Level) ထည့်သွင်းခြင်း
 }
 
 buildscript {
@@ -10,4 +12,3 @@ buildscript {
         classpath(libs.gradle.license.plugin)
     }
 }
-
