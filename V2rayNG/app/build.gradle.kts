5ed999539795e9bfbf28f4a2ee17450b9b502f17
kaungmyatjapan1999-boop <kaungmyatjapan1999@gmail.com>
2026-08-29 15:46:41 +0700

    Add Material Components library dependency

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -140,6 +140,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
