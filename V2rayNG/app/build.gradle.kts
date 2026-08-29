71e8d14f7b13a15c6962348d3eb3839009c4a800
kaungmyatjapan1999-boop <kaungmyatjapan1999@gmail.com>
2026-08-29 15:54:43 +0700

    Add preference-ktx dependency to build.gradle.kts

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -141,6 +141,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
 
     implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     androidTestImplementation(platform("androidx.compose:compose-bom:2025.02.00"))
